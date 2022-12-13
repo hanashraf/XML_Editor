bc753bca62b3db71dbe057edb9d654df6aab838f
Qamar-14 <qamarelgohary@gmail.com>
2022-12-13 17:26:32 +0200

    added functions

--- XML_Editor/consistency.cpp
+++ XML_Editor/consistency.cpp
@@ -210,5 +210,98 @@ bool check_consistency(vector<string>& XML_Vector) {
     return true;
 
 }
+void fix_errors(vector<string>& XML_Vector) {
+    stack<string> stack_tag;
+    stack<int> count;
+    string tag;
+    string str;
+    int n = 0;
+    for (int i = 0; i < XML_Vector.size(); i++)
+    {
+        if ((XML_Vector[i] == "") || (((XML_Vector[i][1] == '?') || (XML_Vector[i][1] == '!')) && XML_Vector[i][0] == '<')
+            || ((XML_Vector[i].find("<") == -1) && (XML_Vector[i].find(">") != -1))
+            || ((XML_Vector[i].find(">") == -1) && (XML_Vector[i].find("<") != -1)))
+        {
+            continue;// if the 1st line is empty or contains "?" or "!" or has a opening tag without a closing tag or vice versa skip line
 
+        }
+        while (n < XML_Vector[i].length())
+        {
+
+            if (n >= XML_Vector[i].length()) break; //if n is larger than number of elements in this row go to next row
+
+            while (XML_Vector[i][n] != '<')
+            {
+                if (n >= XML_Vector[i].length()) {
+                    break;
+                }
+                n++;
+            }
+            int startindex = n + 1; //start index is after opening tag
+            n++;
+
+            if (n >= XML_Vector[i].length()) break;
+
+            while (XML_Vector[i][n] != ' ' && XML_Vector[i][n] != '>')
+            {
+                tag += XML_Vector[i][n];
+                n++;
+                if (n >= XML_Vector[i].length()) break;
+            }
+            int endindex = n + 1;// end index is after closing tag
+            n++;
+            str = tag; // update string str with the string detected between an opening and closing tag
+            tag = ""; // clear string tag
+            if (isOpeningTag(str)) // find out if the string found is an opening tag or a closing tag
+            {
+                stack_tag.push(str); // if it's an opening tag push the string into stack_tag
+                count.push(i); // push into stack(count) the number of line you're at
+            }
+            else
+            {
+
+                if (isXMLTagMatched(stack_tag, str)) // else if it's a closing tag check if the top of the stack is equivalent to the current string
+                {
+                    stack_tag.pop(); // pop string from stack
+                    count.pop(); // pop number of line
+                }
+                else
+                {
+                    XML_Vector[i].insert(0, "<" + str.substr(1) + ">"); // else insert string with an opening tag
+
+                }
+
+            }
+
+
+        }
+
+        n = 0;
+
+    }
+    int len = 0;
+    int size = stack_tag.size() - 1;
+    while (len < size)
+    {
+
+        if (XML_Vector[count.top()].find('/') != -1) // if the number of line (pushed to stack(count)) contains "/" in xml file
+        {
+            int index = XML_Vector[count.top()].find('/') - 1; // index = line -1
+            XML_Vector[count.top()].insert(index, "</" + stack_tag.top() + ">"); // insert closing tag at index
+        }
+        else // if the number of line in xml file doesn't contain "/"
+        {
+            XML_Vector[count.top()].append("</" + stack_tag.top() + ">"); //add closing tag
+        }
+        stack_tag.pop();
+        count.pop();
+        len++;
+    }
+    if (!stack_tag.empty()) // if stack is not empty
+    {
+        XML_Vector.push_back("</" + stack_tag.top() + ">"); // add closing tag to stack
+        stack_tag.pop();
+        count.pop();
+    }
+}
 
