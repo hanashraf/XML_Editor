ddf185412df43c20418bcbafcd6ebdbd49f2f304
amrsharaf04 <amribrahim4@hotmail.com>
2022-12-13 17:30:38 +0200

    GUI modified

--- XML_Editor/mainwindow.cpp
+++ XML_Editor/mainwindow.cpp
@@ -13,3 +13,57 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_ErrorCheck_clicked()
+{
+
+}
+
+
+void MainWindow::on_ShowErrors_clicked()
+{
+
+}
+
+
+void MainWindow::on_FixErrors_clicked()
+{
+
+}
+
+
+void MainWindow::on_Prettify_clicked()
+{
+
+}
+
+
+void MainWindow::on_XMLtoJSON_clicked()
+{
+
+}
+
+
+void MainWindow::on_Minify_clicked()
+{
+
+}
+
+
+void MainWindow::on_Compress_clicked()
+{
+
+}
+
+
+void MainWindow::on_Decompress_clicked()
+{
+
+}
+
+
+void MainWindow::on_DrawGraph_clicked()
+{
+
+}
+
