0156d02d0737367fc18cd8ee54739a1ba4a84c44
Hana Ashraf <hana2002.ashraf@gmail.com>
2022-12-13 14:31:06 +0200

    adding isOpeningTag function

--- XML_Editor/consistency.cpp
+++ XML_Editor/consistency.cpp
@@ -6,12 +6,8 @@ consistency::consistency()
 
 }
 
-<<<<<<< HEAD
 bool isOpeningTag(string tag)
 {
     return (tag[0] != '/');
-=======
-bool areMatchingTags(string tag1, string tag2) {
-    return (tag1 == tag2.substr(1));
->>>>>>> refs/remotes/origin/main
+
 }
