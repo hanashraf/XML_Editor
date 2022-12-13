d80fe05cac87849a738257c0daf1b1a01b289119
Hana Ashraf <hana2002.ashraf@gmail.com>
2022-12-13 16:30:52 +0200

    adding comments to check_consistency function

--- XML_Editor/consistency.cpp
+++ XML_Editor/consistency.cpp
@@ -143,7 +143,7 @@ bool check_consistency(vector<string>& XML_Vector) {
 
     for (int i = 0; i < XML_Vector.size(); i++)
     {
-
+        //if the line does not contain any tags or starts with "" or ? or ! , skip it
         if ((XML_Vector[i] == "") || (((XML_Vector[i][1] == '?') || (XML_Vector[i][1] == '!')) && XML_Vector[i][0] == '<')
             || ((XML_Vector[i].find("<") == -1) && (XML_Vector[i].find(">") != -1))
             || ((XML_Vector[i].find(">") == -1) && (XML_Vector[i].find("<") != -1)))
@@ -151,48 +151,50 @@ bool check_consistency(vector<string>& XML_Vector) {
             continue;
 
         }
-
+        // as long as n is less than XML_Vector[i].length()
         while (n < XML_Vector[i].length())
         {
 
             if (n >= XML_Vector[i].length()) {
-                break;
+                break;//when n is greater than or equal XML_Vector[i].length(), get out of the loop
             }
-            while (XML_Vector[i][n] != '<')
+            while (XML_Vector[i][n] != '<') //iterrate as long as XML_Vector[i][n] is not an openning tag
             {
                 if (n >= XML_Vector[i].length()) {
-                    break;
+                    break;//when n is greater than or equal XML_Vector[i].length(), get out of the loop
                 }
-                n++;
+                n++;//every time we iterrate and XML_Vector[i][n] is not an openning tag,increase n by 1 till n is greater than or equal XML_Vector[i].length()
 
             }
             n++;
             if (n >= XML_Vector[i].length()) {
-                break;
+                break;//when n is greater than or equal XML_Vector[i].length(), get out of the loop
             }
             while (XML_Vector[i][n] != ' ' && XML_Vector[i][n] != '>') {
-                tag += XML_Vector[i][n];
+                tag += XML_Vector[i][n];//As long as, XML_Vector[i][n] not equal space or >, this means we have not reached the end of the tag,so string tag will be equal to what is inside XML_Vector[i][n]
                 if (n >= XML_Vector[i].length()) {
-                    break;
+                    break;//when n is greater than or equal XML_Vector[i].length(), get out of the loop
                 }
-                n++;
+                n++;//every time we iterrate and XML_Vector[i][n] not equal space or >,increase n by 1 till n is greater than or equal XML_Vector[i].length()
             }
 
-            n++;
-            str = tag;
-            tag = "";
-            if (isOpeningTag(str)) {
-                s.push(str);
+            n++;//increase n by 1
+            str = tag;//let sting str defined above equal to tag
+            tag = "";//tag will be equal to null
+            if (isOpeningTag(str)) //check whether the str is an openning tag or not
+            {
+                s.push(str);//if yes,push the openning tag into the stack
             }
 
-            else {
-                if (isXMLTagMatched(s, str))
+            else //if it is not an openning tag, we will check it against the closing tag
+            {
+                if (isXMLTagMatched(s, str))//if the openning tag matches the closing tag, pop the openning tag
                 {
                     s.pop();
                 }
                 else
                 {
-                    return false;
+                    return false;//if the openning tag does not match the closing tag, return false beacause the XML file is inconsistent
                 }
 
             }
@@ -200,10 +202,10 @@ bool check_consistency(vector<string>& XML_Vector) {
 
         }
 
-        n = 0;
+        n = 0;//after getting out of the first while loop,we will make n=0 again as in the start of the function
 
     }
-    if (!s.empty()) {
+    if (!s.empty()) { //if the stack is not empty, then the XML file is inconsistent
         return false;
     }
 
