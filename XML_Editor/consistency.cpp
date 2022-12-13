70a03d3bccb7ae3fe9a2cacfdbb1ad1a080cf77e
Qamar-14 <qamarelgohary@gmail.com>
2022-12-13 14:38:34 +0200

    edited files

--- XML_Editor/consistency.cpp
+++ XML_Editor/consistency.cpp
@@ -15,3 +15,16 @@ bool isOpeningTag(string tag)
 bool areMatchingTags(string tag1, string tag2) {
     return (tag1 == tag2.substr(1));
 }
+
+bool isXMLTagMatched(stack<string> stack_tag, string tag) {
+
+    if (stack_tag.empty()) {
+        return false;
+    }
+    if (!areMatchingTags(stack_tag.top(), tag)) {
+        return false;
+    }
+
+    return true;
+
+}
