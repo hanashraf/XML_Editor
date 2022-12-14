e387f775a18b995ac964dafc25121874742272d1
AhmedAref <ahmed.aref.omaira@gmail.com>
2022-12-14 13:03:25 +0200

    I've implemented the minifying function to remove tabs and spaces from the xml file

--- XML_Editor/formating_and_minifying.cpp
+++ XML_Editor/formating_and_minifying.cpp
@@ -32,3 +32,42 @@ bool readFile(string fileName, vector<string>& xml)
 	in.close();
 	return true;
 }
+
+string minifying_func(vector<string>& xml)
+{
+	//string fady hrg3 feeh al result ale 3mltha be minifying nafsahaa
+	string result = "";
+	// el variable i dah ale h-loop beeh 3ala kol al elements elly fel array element by element y3ne line by line
+	for (int i = 0; i < xml.size(); i++)
+	{
+		// el variable j dah ale h-loop beh 3al line al m3aya (element) lahdd akhroo
+		for (int j = 0; j < xml[i].size(); j++)
+		{
+			// hafdal a-loop 3al line lahd mal charachter ale ana wa2f 3ndo msh be space fadya ' ' w mesh be tab ( asci of tab space equals 9 decimal )
+			if (xml[i][j] != ' ' && xml[i][j] != 9)
+			{
+				//khale al xml[i] bysawe al xml[i] mn awel al charachters lahd al akher 
+				xml[i] = xml[i].substr(j, xml[i].size() - j);
+				break;
+			}
+		}
+		// variable j dh h-loop beeh 3al line mn awel charachter mawgud fel akher lahdd al awel mel shemal  
+		for (int j = xml[i].size() - 1; j >= 0; j--)
+		{
+			// law al charachter ale ana wa2f 3ndo msh be space fadya ' ' w mesh be tab ( asci of tab space equals 9 decimal )
+			if (xml[i][j] != ' ' && xml[i][j] != 9)
+			{
+				//khale al xml[i] mn awel el charachters lahd akher charachter abl awel space
+				xml[i] = xml[i].substr(0, j + 1);
+				break;
+			}
+		}
+		result += xml[i];
+	}
+
+	string file_name = "test_result.txt"; // this file name must be created and located in your project folder
+	vector<string> res; // array to store the xml lines
+	res.push_back(result); // to store the lines line by line
+	writeFile(file_name, res); // to save the xml lines as line by line in the .txt file 
+	return result; // return the whole xml file as one 
+}
