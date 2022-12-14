674e5793de6095e053f79e79d8a417938d965c4f
AhmedAref <ahmed.aref.omaira@gmail.com>
2022-12-14 12:57:27 +0200

    I've implemented the writeFile function to read files

--- XML_Editor/formating_and_minifying.cpp
+++ XML_Editor/formating_and_minifying.cpp
@@ -1,6 +1,16 @@
-#include "formating_and_minifying.h"
-
-Formating_and_Minifying::Formating_and_Minifying()
-{
-
+#include "Formating_and_minifying.h"
+bool writeFile(string fileName, const vector<string>& xml) {
+	fstream out;
+	out.open(fileName, ios::out);
+	if (!out) {
+		cout << "Cannot open the File : " << fileName << endl;
+		return false;
+	}
+	if (out.is_open()) {
+		for (int i = 0; i < (int)xml.size(); i++) {
+			out << xml[i] << "\n";
+		}
+	}
+	out.close();
+	return true;
 }
