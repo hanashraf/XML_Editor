ec60e89092686f5bbf48cfddfaf7f370bf3fac16
Qamar-14 <qamarelgohary@gmail.com>
2023-02-02 16:18:54 +0200

    add header file for compressioncoding file

--- XML_Editor/compressioncoding.h
+++ XML_Editor/compressioncoding.h
@@ -1,11 +1,38 @@
-#ifndef COMPRESSIONCODING_H
-#define COMPRESSIONCODING_H
-
-
-class compressionCoding
+#ifndef COMPRESSIONCODING_H_
+#define COMPRESSIONCODING_H_
+#include "huffTree.h"
+#include <fstream>
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+class CompressionCoding : HuffTree
 {
-public:
-    compressionCoding();
-};
+private:
+    fstream in_file;
+    fstream out_file;
+    string input_file_name;
+    string output_file_name;
+    HuffNode *root_node;
+    char id;
+    void createPriorityQueue();
+    void createHuffmanTree();
+    void traverse(HuffNode *node, string code);
+    void setFixedStringForm(string &input_string);
+    void appendCodeOfEachChar(string &input_string);
+    int binaryToDecimal(string in);
 
-#endif // COMPRESSIONCODING_H
+    class compare
+    {
+    public:
+        bool operator()(HuffNode *child1, HuffNode *child2) const
+        {
+            return child1->getFrequency() > child2->getFrequency();
+        }
+    };
+    priority_queue<HuffNode *, vector<HuffNode *>, compare> P_Q;
+
+public:
+    CompressionCoding(string input_file_name, string output_file_name);
+};
+#endif // COMPRESSIONCODING_
