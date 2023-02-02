e01262c342c22dd8e0353ae2212ade1e6ee628f3
Qamar-14 <qamarelgohary@gmail.com>
2023-02-02 16:10:37 +0200

    create prioriy queue

--- XML_Editor/compressioncoding.cpp
+++ XML_Editor/compressioncoding.cpp
@@ -30,3 +30,26 @@ int CompressionCoding::binaryToDecimal(string number)
     }
     return result;
 }
+// this functions creates priority queue and the comparison of the frequency of each char of the nodes
+
+void CompressionCoding ::createPriorityQueue()
+{
+    in_file.open(input_file_name, ios::in); // open the file
+    in_file.get(id);                        // get first char in the file
+    while (!in_file.eof())
+    {
+        // set the frequency of each char and add it to the array of nodes
+        array_of_nodes[id]->setFrequency((array_of_nodes[id]->getFrequency()) + 1);
+        in_file.get(id); // read next char
+    }
+    in_file.close();
+
+    for (int i = 0; i < 128; i++)
+    {
+        if (array_of_nodes[i]->getFrequency())
+        {
+            // pushing values to the priority queue according to the frequency of each char
+            P_Q.push((HuffNode *)array_of_nodes[i]);
+        }
+    }
+}
