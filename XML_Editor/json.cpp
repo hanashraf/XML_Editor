b227d6cf57f16fec4fe900b58a9944bf0b879f6f
Qamar-14 <qamarelgohary@gmail.com>
2023-02-02 19:40:07 +0200

    adding json.cpp file

--- XML_Editor/json.cpp
+++ XML_Editor/json.cpp
@@ -1,6 +1,20 @@
 #include "json.h"
+//this function check if a node is an attribute(value) or not
+bool node::not_attribute(){
+    if(attr.empty()== 1)
+        return 1;
+    return 0;
+}
 
-json::json()
+int node::get_parent_index()
 {
-
+    int num=0;
+    node *temp=new node;
+    temp=previous;
+    while(temp!=nullptr)
+    {
+        num++;
+        temp=temp->previous;
+    }
+    return num;
 }
