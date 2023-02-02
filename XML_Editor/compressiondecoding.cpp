0ad912ea2e5265f93b27eb298c8b5b8e93a6f323
Hana Ashraf <hana2002.ashraf@gmail.com>
2023-02-02 17:47:34 +0200

    adding constuctor to compression decoding

--- XML_Editor/compressiondecoding.cpp
+++ XML_Editor/compressiondecoding.cpp
@@ -71,3 +71,58 @@ void compressiondecoding::createHuffmanTree()
     }
     in_file.close();                                          // closing the input file
 }
+compressiondecoding::compressiondecoding(string input_file_name, string output_file_name)
+{
+    this->input_file_name = input_file_name;
+    this->output_file_name = output_file_name;
+    createNodeArray();
+    createHuffmanTree();
+
+    in_file.open(input_file_name, ios::in | ios::binary);           // opening the input file
+    out_file.open(output_file_name, ios::out);                      // opening the output file
+    unsigned char huffTreeSize;
+    char counter;
+    in_file.read(reinterpret_cast<char *>(&huffTreeSize), 1);       // reading the size of the tree
+    in_file.seekg(-1, ios::end);                                    // jumping to the last one byte to get the number of zeroes appended to the string at the end
+
+    in_file.read(&counter, 1);                                      // reading the number of zeroes appended to the string at the end
+    in_file.seekg(1 + 17 * huffTreeSize, ios::beg);                 // jumping to the position where the text starts
+
+    vector<unsigned char> text_vec;
+    unsigned char textsec;
+    in_file.read(reinterpret_cast<char *>(&textsec), 1);            // reading the first character code
+
+    while (!in_file.eof())                                          // checking whether it is the end of the file ot not
+    {                                                               // geting the text byte by byte
+        text_vec.push_back(textsec);                                // pushing the character read from input file in the new text vector
+        in_file.read(reinterpret_cast<char *>(&textsec), 1);        // reading the next character code
+    }                                                               // loop till we reach the end of the file
+
+    HuffNode *current_ptr = root_node;                              // initializing the pointer to point to the root node
+    string path;
+    for (int i = 0; i <(int) text_vec.size() - 1; i++)
+    {                                                               // translating the huffman code
+        path = decimalToBinary(text_vec[i]);                        // converting characters codes in text vector from decimal to binary and save it in a string path
+
+        if (i ==(int) text_vec.size() - 2)
+            path = path.substr(0, 8 - counter);
+
+        for (int j = 0; j <(int) path.size(); j++)
+        {
+            if (path[j] == '0')
+            {
+                current_ptr = current_ptr->getLeft();               // letting the pointer to point to the left node
+            }
+            else
+                current_ptr = current_ptr->getRight();              // letting the pointer to point to the left node
+
+            if (current_ptr->getLeft() == NULL && current_ptr->getRight() == NULL)
+            {
+                out_file.put(current_ptr->getId());                 // writing its id "character" in the output file
+                current_ptr = root_node;                            // pointing again to the root node
+            }
+        }
+    }
+    in_file.close();                                                // closing input file
+    out_file.close();                                               // closing output file
+}
