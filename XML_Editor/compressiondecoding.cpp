4c007f8b26164f6631e735c0125bcb49cf2e7c14
Hana Ashraf <hana2002.ashraf@gmail.com>
2023-02-02 17:24:37 +0200

    adding decimal to binary function for compression decoding

--- XML_Editor/compressiondecoding.cpp
+++ XML_Editor/compressiondecoding.cpp
@@ -1,3 +1,19 @@
 #include "compressiondecoding.h"
+#include <iostream>
+using namespace std;
+string compressiondecoding::decimalToBinary(int number)
+{
+    int i = 0;
+    string binaryNumber = "", zeroes;
+    while (number > 0)
+    {
+        binaryNumber = (number % 2 == 0 ? "0" : "1") + binaryNumber;
 
+        number /= 2;
+        i++;
+    }
+    zeroes.append(8 - binaryNumber.size(), '0');
+    binaryNumber.insert(0, zeroes);
+    return binaryNumber;
+}
 
